c3f34394d3dbd4558df97bba00cc40986822a2fc
wulikanhua <1113513062@qq.com>
2017-07-09 15:28:53 +0800

    练习5-9

--- chapter5/5-9.c
+++ chapter5/5-9.c
@@ -6,6 +6,23 @@ static char daytab[2][13] = {
 
 int day_of_year(int year, int month, int day)
 {
-    int i, leap;
+    int leap;
+    char *p;
+
+    leap = year%4 == 0 && year%100 != 0 && year%400==0;
+    p = daytab[leap];
+    while (--month)
+        day += *++p;
+    return day;
     
+}
+
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+    int leap;
+    char *p;
+    while (yearday > *++p)
+        yearday -= *p;
+    *pmonth = p - *(daytab+leap);
+    *pday = yearday;
 }
