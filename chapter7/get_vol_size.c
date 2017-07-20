d60859f691901bd9efa3e9d0d8fccfa452c9095c
wulikanhua <1113513062@qq.com>
2017-07-20 11:45:55 +0800

    获取卷大小

---
+++ chapter7/get_vol_size.c
@@ -0,0 +1,29 @@
+/*************************************************************************
+	> File Name: get_vol_size.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2017年07月20日 星期四 11时32分28秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<sys/types.h>  /*提供类型pid_t,size_t的定义*/
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+unsigned long long getVolumeDiskSize(char *diskName) {
+    int fd = open(diskName, O_RDONLY);
+    unsigned long long size = -1;
+    if (fd < 0) {
+     return -1;        
+    }
+    size = lseek(fd, 0, SEEK_END) - lseek(fd, 0, SEEK_SET);
+    close(fd);
+    return size;
+
+}
+
+int main()
+{
+    unsigned long long size = getVolumeDiskSize("/dev/vda1");
+    printf("%lld", size);
+}
