25a40a32fd66ef029842c86dfd81d7e1525f7b94
wulikanhua <1113513062@qq.com>
2017-08-10 21:24:11 +0800

    优化获取UUID

--- chapter8/uuid.c
+++ chapter8/uuid.c
@@ -7,46 +7,57 @@
 
 #include<stdio.h>
 #include<string.h>
-char * vol_get_uuid(char * vol_path)
+#include<stdlib.h>
+#define MAX_SHELL_STR_LEN 128
+#define VOL_UUID_LEN 64
+
+
+
+
+int  vol_get_uuid(char * vol_path, char * uuid)
 {
-    int i;
-    char cmd[128] = {0};
-    char * uuid[64];
+    //static char uuid[VOL_UUID_LEN] = {0};
+    int i = 0;
+    char cmd[MAX_SHELL_STR_LEN] = {0};
     FILE * file_temp = NULL;
     char parse_buf[128] = {0};
     char * str = NULL;
-    char * strs[3] = {0};
+    char * strs[6] = {0};
     char * attribute;
     char * value;
 
-    snprintf(cmd, 128, "blkid %s", vol_path);
+    snprintf(cmd, MAX_SHELL_STR_LEN, "blkid %s", vol_path);
     file_temp = popen(cmd, "r");
     if (NULL == file_temp) {
         perror("pipe open failed");
-        return NULL;
+        return -1;
 
     }
 
     if(NULL != fgets(parse_buf, 128, file_temp)) {
         str = parse_buf;
         while(NULL != (strs[i] = strtok(str," "))){
+            printf("%s",strs[i]);
             i++;
             str = NULL;
 
         }
         attribute = strtok(strs[1], "=");
-        value     = strtok(NULL, "=");
-        memcpy(uuid, value, 64);
-        return  *uuid;
+        value   = strtok(NULL, "=");
+        strncpy(uuid, value, VOL_UUID_LEN);
+        return  0;
     }
-
+    return 1;
 }
+
 
 
 int main()
 {
-    char * uuid = vol_get_uuid("/dev/vad1"); 
-    //strncpy(uuid,vol_get_uuid("/dev/vda1"),64);
-
-    printf("%s", uuid);      
+    int  sys_ret = 0;
+    char * vol_path = "/dev/vda1";
+    char * uuid = NULL;
+    uuid = (char *)malloc(sizeof(char)*VOL_UUID_LEN);
+    sys_ret = vol_get_uuid(vol_path, uuid);
+    printf("%s\n",uuid);
 }
