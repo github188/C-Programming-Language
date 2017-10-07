8bc2addd2c1937ff60ce4861c49b4816c35f58fe
wulikanhua <1113513062@qq.com>
2017-10-07 16:26:27 +0800

    打印*

--- chapter13/mystery.c
+++ chapter13/mystery.c
@@ -1,4 +1,22 @@
 //
 // Created by 谭伟 on 2017/10/6.
 //
+#include <stdio.h>
+#include <stdlib.h>
 
+void mystery(int n)
+{
+    n += 5;
+    n /= 10;
+    printf("%s\n", "**********" + 10 - n);
+}
+
+int main(int argc, char ** argv)
+{
+    while(*++argv != NULL) {
+        printf("%s\n", *argv);
+        mystery(atoi(*argv));
+//        printf("%d\n", **argv);
+    }
+
+}
