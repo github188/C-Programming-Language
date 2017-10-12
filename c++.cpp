c8fb37f3c6987bfdaa1432420b68732014c0ca22
wulikanhua <1113513062@qq.com>
2017-10-12 15:30:19 +0000

    简单的C++

---
+++ c++.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: c++.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 03 Oct 2017 12:20:41 PM UTC
+ ************************************************************************/
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout << "请输入一个十进制整数" << endl;
+    cin >> x;
+    cout << oct << x;
+    cout << dec << x;
+    cout << hex << x;
+    system("pause");
+    bool y;
+    y = false;
+    cout << "请输入一个布尔值" << endl;
+    cin >> y;
+    cout << y << endl;
+
+}
