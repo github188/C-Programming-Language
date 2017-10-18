ae4abbeefc8f4fb1bdcfb4b61c1f9f42933032c4
谭伟 <1113513062@qq.com>
2017-10-18 17:35:08 +0800

    释放结构体指针所指向的内存

---
+++ malloc.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define PATH_NAME_LEN 128
+#define TYPE_OF_LV    48
+
+
+typedef struct  lv_info
+{
+    char lv_path[PATH_NAME_LEN];           /* 卷路径 */
+    char lv_mount_path[PATH_NAME_LEN];     /* 卷挂载路径 */
+    char lv_type[TYPE_OF_LV];              /* 卷类型 */
+    unsigned long long lv_total_size;      /* 卷总空间 */
+    unsigned long long lv_left_size;       /* 卷空余空间 */
+}lv_info, *pt_lv_info;
+
+int son(pt_lv_info * p_lv)
+{
+    *p_lv = (pt_lv_info)malloc(sizeof(lv_info)*32);
+
+    if (*p_lv == NULL) {
+        return 0;
+    }else {
+        return 1;
+    }
+}
+
+int main()
+{
+    int ret;
+    pt_lv_info  p_lv_info = NULL;
+
+    printf("%p\n", p_lv_info);
+    ret = son(&p_lv_info);
+    if(ret) {
+
+        printf("%p\n", p_lv_info);
+
+       free(p_lv_info);
+       p_lv_info = NULL;
+        printf("%p\n", p_lv_info);
+    }
+    return 0;
+}
