37b3b278c1fd65729d1ca04d18edd1f593e69c08
wulikanhua <1113513062@qq.com>
2018-01-11 14:45:49 +0800

    信号操作

---
+++ signal/singal.c
@@ -0,0 +1,36 @@
+#include<signal.h>
+#include<stdio.h>
+#include<unistd.h>
+
+void printsigset(const sigset_t *set)
+{
+    int i;
+    for (i=0; i<32;i++)
+    {
+        if (sigismember(set, i)==1)
+        {
+            putchar('1');
+        }
+        else
+        {
+            putchar('0');
+        }
+    }
+    puts("");
+}
+
+int main()
+{
+    sigset_t s,p;
+    sigemptyset(&s);
+    sigaddset(&s, SIGINT);
+    sigprocmask(SIG_BLOCK, &s, NULL);
+    while(1)
+    {
+        sigpending(&p);
+        printsigset(&p);
+        sleep(1);
+    }
+    return 0;
+}
+
